ab9b98a4bee433ece9752288820a6aec6797d743
Stephen Magill <stephen.magill@gmail.com>
2021-06-02 11:35:36 -0400

    Stage initial code

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -48,15 +48,8 @@ public class User {
       stmt = cxn.createStatement();
       System.out.println("Opened database successfully");
 
-      String query = "select * from users where username = '" + encodeForSQL(un) + "' limit 1";
-      System.out.println(query);
-      ResultSet rs = stmt.executeQuery(query);
-      if (rs.next()) {
-        String user_id = rs.getString("user_id");
-        String username = rs.getString("username");
-        String password = rs.getString("password");
-        user = new User(user_id, username, password);
-      }
+      // TODO: query database
+      
       cxn.close();
     } catch (Exception e) {
       e.printStackTrace();
